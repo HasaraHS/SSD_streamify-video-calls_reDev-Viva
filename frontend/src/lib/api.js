c303dd421f0166686f97ced74560aaedadc652e2
Priyanka Prabashwara <prabashwara65@gmail.com>
2025-10-14 11:59:57 +0530

    fixing login and redirect

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -20,7 +20,7 @@ export const getAuthUser = async () => {
     const res = await axiosInstance.get("/auth/me");
     return res.data;
   } catch (error) {
-    console.log("Error in getAuthUserrrrrrrrr:", error)
+    console.log("Error in getAuthUser:", error)
     return null;
   }
   
